21cbd17e8e6a0d45019332cc5057bc1279669987
Adam Antkowiak <adam.antkowiak93@gmail.com>
2025-09-22 13:13:03 +0200

    create posts service

--- src/app/features/posts/posts.service.ts
+++ src/app/features/posts/posts.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post, User, Comment } from './posts.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
-  
+  private http = inject(HttpClient);
+  private apiUrl = 'https://jsonplaceholder.typicode.com';
+
+  constructor() {}
+
+  getPosts(userId?: number): Observable<Post[]> {
+    let params = new HttpParams();
+    if (userId) {
+      params = params.set('userId', userId.toString());
+    }
+    return this.http.get<Post[]>(`${this.apiUrl}/posts`, { params });
+  }
+
+  getPostDetails(postId: number): Observable<Post> {
+    return this.http.get<Post>(`${this.apiUrl}/posts/${postId}`);
+  }
+
+  getAuthor(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${userId}`);
+  }
+
+  getComments(postId: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${this.apiUrl}/posts/${postId}/comments`);
+  }
 }
