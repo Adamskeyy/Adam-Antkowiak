4684e5d5855635d063aeb82caba0e300922f1df1
Adam Antkowiak <adam.antkowiak93@gmail.com>
2025-09-22 13:36:57 +0200

    display posts titles in posts list

--- src/app/features/posts/posts-list/posts-list.ts
+++ src/app/features/posts/posts-list/posts-list.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { PostsStoreService } from '../../../core/services/posts-store';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './posts-list.html',
-  styleUrl: './posts-list.css'
 })
-export class PostsList {
+export class PostsList implements OnInit {
+  private readonly postsStore = inject(PostsStoreService);
+  protected posts = this.postsStore.posts;
+  protected loading = this.postsStore.loading;
 
+  ngOnInit(): void {
+    this.postsStore.loadPosts();
+  }
 }
