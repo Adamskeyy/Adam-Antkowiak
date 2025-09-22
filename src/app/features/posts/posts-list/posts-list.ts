8aa142fd998919e2e3e5b8fa93df62cc86d5999b
Adam Antkowiak <adam.antkowiak93@gmail.com>
2025-09-22 16:49:50 +0200

    add angular animation when posts appear

--- src/app/features/posts/posts-list/posts-list.ts
+++ src/app/features/posts/posts-list/posts-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PostsStoreService } from '../../../core/services/posts-store.service';
@@ -9,23 +9,43 @@ import { Loader } from '../../../shared/components/loader/loader';
   standalone: true,
   imports: [CommonModule, RouterModule, Loader],
   templateUrl: './posts-list.html',
+  styleUrls: ['./posts-list.css'],
 })
 export class PostsList implements OnInit {
   protected readonly postsStore = inject(PostsStoreService);
-  protected posts = this.postsStore.posts;
   protected loading = this.postsStore.loading;
 
-  public setSearchText = this.postsStore.setSearchText.bind(this.postsStore);
-  public setUserIdFilter = this.postsStore.setUserIdFilter.bind(this.postsStore);
-  public toggleFavoritesOnly = this.postsStore.toggleFavoritesOnly.bind(this.postsStore);
+  protected isAnimating = signal(false);
+  protected posts = computed(() => {
+    if (this.isAnimating()) {
+      return [];
+    }
+    return this.postsStore.posts();
+  });
 
   ngOnInit(): void {
     this.postsStore.loadPosts();
   }
 
+  public setSearchText(text: string): void {
+    this.postsStore.setSearchText(text);
+  }
+
+  public setUserIdFilter(userId: number | null): void {
+    this.postsStore.setUserIdFilter(userId);
+  }
+
+  public toggleFavoritesOnly(): void {
+    this.postsStore.toggleFavoritesOnly();
+    this.isAnimating.set(true);
+    setTimeout(() => {
+      this.isAnimating.set(false);
+    });
+  }
+
   toggleFavorite(postId: number, event: MouseEvent): void {
     event.stopPropagation();
-    const currentPosts = this.posts();
+    const currentPosts = this.postsStore.posts();
     const updatedPosts = currentPosts.map((post) => {
       if (post.id === postId) {
         return { ...post, favorite: !post.favorite };
