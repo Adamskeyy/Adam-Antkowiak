65f1a4216e1567a9777eeb0fe283e9b681d481e0
Adam Antkowiak <adam.antkowiak93@gmail.com>
2025-09-22 15:36:50 +0200

    implement post details

--- src/app/features/posts/post-details/post-details.ts
+++ src/app/features/posts/post-details/post-details.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostDetailsModel } from '../posts.model';
+import { filter, switchMap } from 'rxjs/operators';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { PostsStoreService } from '../../../core/services/posts-store';
+import { Loader } from '../../../shared/components/loader/loader';
 
 @Component({
   selector: 'app-post-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Loader],
   templateUrl: './post-details.html',
-  styleUrl: './post-details.css'
 })
 export class PostDetails {
+  private readonly route = inject(ActivatedRoute);
+  private readonly postsStore = inject(PostsStoreService);
+  private readonly router = inject(Router);
 
+  protected loading = this.postsStore.loading;
+
+  private postId = toSignal(
+    this.route.paramMap.pipe(
+      filter((params) => !!params.get('id')),
+      switchMap((params) => [Number(params.get('id'))]),
+    ),
+  );
+
+  public postDetailsData: Signal<PostDetailsModel | undefined> = toSignal(
+    toObservable(this.postId).pipe(
+      filter((id) => id !== undefined),
+      switchMap((id) => this.postsStore.loadPostDetails(id as number)),
+    ),
+  );
+
+  public goBack() {
+    this.router.navigate(['/posts']);
+  }
 }
